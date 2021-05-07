34d2665296fb3c0ccd1a775d19349f5191dd80df
Aivlad <aivlad@internet.ru>
2021-05-07 21:15:10 +0300

    Animations and their management

--- Assets/Simple elements/Scripts/Player.cs
+++ Assets/Simple elements/Scripts/Player.cs
@@ -7,17 +7,28 @@ public class Player : MonoBehaviour
     public float speed;
     public float jumpForece;
 
-    private bool isGrounded;
+    public bool isGrounded;
     private Rigidbody2D rigitbody2d;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
 
     private void Start()
     {
-        // заносим данные с объекта вв переменную
+        // заносим данные с объекта в переменные
         rigitbody2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+        // если нажали клавишу Space
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
         // сохраняем координаты объекта
         Vector3 position = transform.position;
 
@@ -27,33 +38,57 @@ public class Player : MonoBehaviour
         // переносим новую позицию на объект
         transform.position = position;
 
-        // если нажали клавишу Space
-        if (Input.GetKey(KeyCode.Space)) 
+        // проверка на движение
+        if (Input.GetAxis("Horizontal") != 0)
         {
-            Jump();
+            // проверка в какую сторону движемся
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                spriteRenderer.flipX = true; // горизонтальное отражение спрайтов
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                spriteRenderer.flipX = false; // горизонтальное отражение спрайтов
+            }
+            animator.SetInteger("State", 1);
         }
+        else 
+        {
+            animator.SetInteger("State", 0);
+        }
+
+        if (!isGrounded) 
+        {
+            animator.SetInteger("State", 2);
+        }
+
     }
 
     private void Jump()
     {
-        // проверка: объект стоит на земле?
-        if (isGrounded)
-        {
-            // отмечаем, что объект покинул землю
-            isGrounded = false;
-            // делаем прыжок 
-            rigitbody2d.AddForce(transform.up * jumpForece, ForceMode2D.Impulse);
-        }
+        // делаем прыжок 
+        rigitbody2d.AddForce(transform.up * jumpForece, ForceMode2D.Impulse);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // если есть пересечение с землей
-        if (collision.gameObject.tag == "Ground") 
+        // если входим в область земли
+        if (collision.gameObject.tag == "Ground")
         {
             // ставим флаг: объект на земле
             isGrounded = true;
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // если выходим из области земли
+        if (collision.gameObject.tag == "Ground")
+        {
+            // ставим флаг: объект покинул земле
+            isGrounded = false;
+        }
+
     }
 
 
