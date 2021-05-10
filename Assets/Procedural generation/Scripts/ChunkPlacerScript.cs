814fb092eecaed438c1e91ecc11e8de460daa7ac
Aivlad <aivlad@internet.ru>
2021-05-10 23:52:21 +0300

    Random decorations when generated

--- Assets/Procedural generation/Scripts/ChunkPlacerScript.cs
+++ Assets/Procedural generation/Scripts/ChunkPlacerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ChunkPlacerScript : MonoBehaviour
 {
@@ -25,7 +26,8 @@ public class ChunkPlacerScript : MonoBehaviour
 
     private void SpawnChunk()
     {
-        var newChunk = Instantiate(ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length)]);
+        // var newChunk = Instantiate(ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length)]);
+        var newChunk = Instantiate(GetRandomChunk());
         newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].GetComponent<ChunkScript>().End.position - newChunk.GetComponent<ChunkScript>().Begin.localPosition;
         spawnedChunks.Add(newChunk);
 
@@ -34,5 +36,32 @@ public class ChunkPlacerScript : MonoBehaviour
             Destroy(spawnedChunks[0]);
             spawnedChunks.RemoveAt(0);
         }
+    }
+
+    private GameObject GetRandomChunk()
+    {
+        // шансы всех кусков
+        List<float> chanes = new List<float>();
+        for (int i = 0; i < ChunkPrefabs.Length; i++)
+        {
+            chanes.Add(ChunkPrefabs[i].GetComponent<ChunkScript>().ChanceFromDistance.Evaluate(Player.transform.position.x));
+        }
+
+        // считаем сумму и получаем значение от 0 до этой суммы
+        float value = Random.Range(0, chanes.Sum());
+
+        // определяем в какой кусок попали
+        float sum = 0;
+        for (int i = 0; i < chanes.Count; i++)
+        {
+            sum += chanes[i];
+            if (value < sum)
+            {
+                return ChunkPrefabs[i];
+            }
+        }
+
+        // формально: недостижимый выход, т.к. дальше цикла проходить мы не должны
+        return ChunkPrefabs[ChunkPrefabs.Length - 1];
     }
 }
--- Assets/Procedural generation/Scripts/ChunkScript.cs
+++ Assets/Procedural generation/Scripts/ChunkScript.cs
@@ -6,4 +6,18 @@ public class ChunkScript : MonoBehaviour
 {
     public Transform Begin;
     public Transform End;
+
+    public Material[] BlockMaterials;    // material, texture, mesh ...
+
+    public AnimationCurve ChanceFromDistance;   // шанс в зависимости от пройденной дистанции
+
+    private void Start()
+    {
+        foreach (var filter in GetComponentsInChildren<MeshRenderer>()) 
+        {
+            filter.material = BlockMaterials[Random.Range(0, BlockMaterials.Length)];   // случайны материал
+            filter.transform.rotation = Quaternion.Euler(0, Random.Range(0, 4) * 90, 0);    // случайный угол поворота
+        }
+    }
+
 }
